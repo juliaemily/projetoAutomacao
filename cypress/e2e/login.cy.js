b8375e80be6a8332b9856559d626e9d3f5341214
juliaemily <julia.andrade@madeiramadeira.com>
2025-01-22 18:47:24 -0300

    Meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,7 @@
+describe('Iterações Login', () => {
+
+    it('Login', () => {
+     
+    })
+  }
+)
---
+++ cypress/pages/exampleLoginPage.js
@@ -0,0 +1,16 @@
+module.exports = {
+    fields: {
+        fieldEmail: '[id="E-mail"]',
+        fieldPassword: '[id="Senha"]',
+    },
+
+    clickable: {
+        buttonContinue: 'Continuar',
+        buttonLogin: 'Entrar',
+    },
+
+
+    values: {        
+        login: "/verificar"
+    },
+}
