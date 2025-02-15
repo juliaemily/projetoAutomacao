7c093dca32daeecb76622815d15f0e297625763b
Marcelly Costa <marcelly_da_costa@hotmail.com>
2025-02-15 13:30:23 -0300

    Continuação da automação Performance
    - Acrescentado edit e delete no teste;
    - Criadas novas funções na base page;
    - Adicionado biblioteca faker.

--- cypress/e2e/performanceConfigure.cy.js
+++ cypress/e2e/performanceConfigure.cy.js
@@ -1,14 +1,40 @@
 import PerformancePage from "../pages/PerformancePage";
 
-describe('Ensure Successful Completion of All Registrations in the Configure Section', () => {
+describe('Ensure Successful Completion of All Registrations in the Configure/KPIs Section', () => {
   beforeEach(() => {
     cy.setupAndLogin();
   });
 
-  it('Ensure Successful Addition of KPIs', () => {
+  it('Should add a KPI', () => {
     PerformancePage
       .selectPerformance()
+      .selectKPIS()
       .addKPIS()
+      .kpisValidations();
   });
+
+  it('Should edit a KPI', () => {
+    PerformancePage
+      .selectPerformance()
+      .selectKPIS()
+      .editKPIS()
+      .kpisValidations();
+  });
+
+  it('Should delete a KPI using the icon', () => {
+    PerformancePage
+      .selectPerformance()
+      .selectKPIS()
+      .selectDeleteIcon()
+      .deleteItem();
+  });
+
+  it('Should delete a KPI using Deleted Select', () =>{
+    PerformancePage
+      .selectPerformance()
+      .selectKPIS()
+      .selectButtonDelete()
+      .deleteItem();
+  })
 });
 
